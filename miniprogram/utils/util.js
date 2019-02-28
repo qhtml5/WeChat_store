08df616782ce8b903f104de553409c33991a570d
yancey <yancey1204@gmail.com>
2019-02-28 13:44:04 +0800

    checkout authorization status when user enter to me page

--- miniprogram/utils/util.js
+++ miniprogram/utils/util.js
@@ -2,5 +2,25 @@ module.exports = {
   // make price with 2 digits
   formatPrice(price){
     return parseFloat(Math.round(price * 100) / 100).toFixed(2)
-  }
+  },
+
+  getUserInfo() {
+    return new Promise((resolve, reject) => {
+      wx.getSetting({
+        success(res) {
+          if (res.authSetting['scope.userInfo'] === false) {
+            //refuse the authentication
+            reject()
+          } else {
+            wx.getUserInfo({
+              success(res) {
+                const userInfo = res.userInfo
+                resolve(userInfo)
+              }
+            })
+          }
+        }
+      })
+    })
+  },
 }
